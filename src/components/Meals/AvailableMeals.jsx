ef2ebcb5d00b29735fd00260ed7b57b1b6e61cb0
Rerique <efpri.nanda@gmail.com>
2023-01-23 20:28:06 +0700

    adding individual meal items

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,14 +1,23 @@
 import styles from './AvailableMeals.module.css';
 import DUMMY_MEALS from '../../data/meals-data';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 export default function AvailableMeals() {
   const mealsList = DUMMY_MEALS.map((meal) => (
-    <li key={meal.id}>{meal.name}</li>
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
   ));
 
   return (
     <section className={styles.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 }
