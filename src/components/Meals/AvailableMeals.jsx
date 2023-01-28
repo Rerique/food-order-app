c167624a715ae666b55a31dacd65d2e24d4964f3
Rerique <efpri.nanda@gmail.com>
2023-01-28 22:45:59 +0700

    add http request

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,10 +1,45 @@
+import { useCallback, useEffect, useState } from 'react';
 import styles from './AvailableMeals.module.css';
-import DUMMY_MEALS from '../../data/meals-data';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
 export default function AvailableMeals() {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const mealsRequest = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(
+        'https://react-http-request-pract-9cb0f-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json',
+      );
+      const meals = await response.json();
+
+      const mealsData = [];
+      Object.entries(meals).forEach(([key, value]) => {
+        mealsData.push({
+          id: key,
+          name: value.name,
+          description: value.description,
+          price: Number(value.price),
+        });
+      });
+
+      setMeals(mealsData);
+    } catch (err) {
+      setError(err.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    mealsRequest();
+  }, [mealsRequest]);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
@@ -14,9 +49,18 @@ export default function AvailableMeals() {
     />
   ));
 
+  let content = '';
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading ...</p>;
+  }
+
   return (
     <section className={styles.meals}>
       <Card>
+        {isLoading && content}
         <ul>{mealsList}</ul>
       </Card>
     </section>
